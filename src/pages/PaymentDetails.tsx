a46d6155a603a5e06971c870d32fbfcbc1b16f56
Maxim <mladoshin@mail.ru>
2024-07-01 22:37:12 +0300

    feat: changes logic of saving payment details

--- src/pages/PaymentDetails.tsx
+++ src/pages/PaymentDetails.tsx
@@ -2,8 +2,10 @@ import { Button, Input, List, Space } from "antd";
 import React, { useEffect, useState } from "react";
 import { api, withCredentials } from "../services/api";
 import { DeleteOutlined } from "@ant-design/icons";
+import { PaymentDetailType } from "../@types/enum/PaymentDetailType";
 function PaymentDetails() {
-  const [data, setData] = useState<any>({});
+  const [data, setData] = useState<any[]>([]);
+  const visibleData = data.filter((el) => !el.id.includes("del-"));
 
   useEffect(() => {
     fetchData();
@@ -16,35 +18,50 @@ function PaymentDetails() {
     setData(resp.data);
   }
 
-  function addItem(field: "card" | "sbp") {
-    const f = field === "card" ? "card" : "tel";
+  function addItem(type: PaymentDetailType) {
     const tmp = JSON.parse(JSON.stringify(data));
-    tmp[field].push({ bank: "", [f]: "" });
+    tmp.push({
+      bank: "",
+      data: "",
+      priority: 1,
+      type,
+      id: `new-${Date.now()}`,
+    });
     setData(tmp);
   }
 
-  function deleteItem(field: "card" | "sbp", index: number) {
-    const f = field === "card" ? "card" : "tel";
-    const tmp = JSON.parse(JSON.stringify(data));
-    tmp[field].splice(index, 1);
-    setData(tmp);
+  function deleteItem(id: string) {
+    const index = data.findIndex((el) => el.id === id);
+    if (index !== -1) {
+      const tmp = [...data];
+      if (tmp[index].id.includes("new-")) {
+        tmp.splice(index, 1);
+      } else {
+        tmp[index].id = `del-${id}`;
+      }
+      setData(tmp);
+    }
   }
 
   function handleChangeField(
-    type: "card" | "sbp",
-    index: number,
-    field: "bank" | "card" | "tel",
-    value: string
+    id: string,
+    field: "data" | "priority" | "bank",
+    value: string | number
   ) {
-    const tmp = JSON.parse(JSON.stringify(data));
-    tmp[type][index][field] = value;
-    setData(tmp);
+    const index = data.findIndex((el) => el.id === id);
+    if (index !== -1) {
+      const tmp = [...data];
+      tmp[index][field] = value;
+      setData(tmp);
+    }
   }
 
   async function savePaymentDetails() {
+    // console.log(data);
+    // return;
     try {
       await withCredentials((headers) =>
-        api.post("admin/payment-details", data, headers)
+        api.post("admin/payment-details", { data }, headers)
       );
       await fetchData();
     } catch (error) {
@@ -58,29 +75,48 @@ function PaymentDetails() {
         size="large"
         header={<h3>Карты</h3>}
         bordered
-        dataSource={data.card}
-        footer={<Button onClick={() => addItem("card")}>Добавить карту</Button>}
+        dataSource={visibleData?.filter(
+          (dataItem) => dataItem.type === PaymentDetailType.CARD
+        )}
+        footer={
+          <Button onClick={() => addItem(PaymentDetailType.CARD)}>
+            Добавить карту
+          </Button>
+        }
         renderItem={(item: any, index) => (
           <List.Item>
             <Input
               value={item.bank}
               onChange={(e) =>
-                handleChangeField("card", index, "bank", e.target.value)
+                handleChangeField(item.id, "bank", e.target.value)
               }
             />
 
             <Input
               style={{ marginLeft: 20 }}
-              value={item.card}
+              value={item.data}
               onChange={(e) =>
-                handleChangeField("card", index, "card", e.target.value)
+                handleChangeField(item.id, "data", e.target.value)
               }
             />
+
+            <Input
+              placeholder="Приоритет"
+              type="number"
+              max={10}
+              min={0}
+              style={{ marginLeft: 20 }}
+              value={item.priority}
+              onChange={(e) =>
+                handleChangeField(item.id, "priority", +e.target.value)
+              }
+            />
+
             <div style={{ marginLeft: 20 }}>
               <Button
                 icon={<DeleteOutlined />}
                 danger
-                onClick={() => deleteItem("card", index)}
+                onClick={() => deleteItem(item.id)}
               />
             </div>
           </List.Item>
@@ -91,30 +127,49 @@ function PaymentDetails() {
         style={{ marginTop: 50 }}
         size="large"
         header={<h3>СБП</h3>}
-        footer={<Button onClick={() => addItem("sbp")}>Добавить СБП</Button>}
+        footer={
+          <Button onClick={() => addItem(PaymentDetailType.SBP)}>
+            Добавить СБП
+          </Button>
+        }
         bordered
-        dataSource={data.sbp}
+        dataSource={visibleData?.filter(
+          (dataItem) => dataItem.type === PaymentDetailType.SBP
+        )}
         renderItem={(item: any, index) => (
           <List.Item>
             <Input
               value={item.bank}
               onChange={(e) =>
-                handleChangeField("sbp", index, "bank", e.target.value)
+                handleChangeField(item.id, "bank", e.target.value)
               }
             />
 
             <Input
               style={{ marginLeft: 20 }}
-              value={item.tel}
+              value={item.data}
               onChange={(e) =>
-                handleChangeField("sbp", index, "tel", e.target.value)
+                handleChangeField(item.id, "data", e.target.value)
               }
             />
+
+            <Input
+              placeholder="Приоритет"
+              type="number"
+              max={10}
+              min={0}
+              style={{ marginLeft: 20 }}
+              value={item.priority}
+              onChange={(e) =>
+                handleChangeField(item.id, "priority", +e.target.value)
+              }
+            />
+
             <div style={{ marginLeft: 20 }}>
               <Button
                 icon={<DeleteOutlined />}
                 danger
-                onClick={() => deleteItem("sbp", index)}
+                onClick={() => deleteItem(item.id)}
               />
             </div>
           </List.Item>
