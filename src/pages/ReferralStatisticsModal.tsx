da565a9c952e08cd7ea78fa35fca626cb4f5a613
Vovarama1992 <vovayhh9988@gmail.com>
2024-09-13 14:31:59 +0300

    no-headers

--- src/pages/ReferralStatisticsModal.tsx
+++ src/pages/ReferralStatisticsModal.tsx
@@ -24,8 +24,7 @@ const ReferralStatisticsModal: React.FC<ReferralStatisticsModalProps> = ({ userI
       const headers = { Authorization: `Bearer ${token}` };
 
       const resp = await api.get(`/user/referrals`, {
-        headers, 
-        params: { userId },
+         params: { userId },
       });
 
       setReferralData(resp.data); 
