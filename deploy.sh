da7087433f4236dabeaf8c10d71c351f35597690
Maxim Ladoshin <66252439+mladoshin@users.noreply.github.com>
2024-09-18 00:12:16 +0300

    Fix deploy script (#4)
    * fix: changed api url
    
    * feat: added error handler on login
    
    * fix: fixed env
    
    * fix: fixed deploy script
    
    * fix: fixed deploy script
    
    * fix: fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
+#!/bin/sh
+mode=$1
+if [ -z "$mode" ]; then 
+echo Error! Select build mode: build or staging
+exit 1
+fi
+
 git reset --hard
 git pull
-npm run build
+npm run build:$mode
 rm -rf /usr/share/nginx/admin_frontend
 cp -r dist/ /usr/share/nginx/admin_frontend
 echo Successful
