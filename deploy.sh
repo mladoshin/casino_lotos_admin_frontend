c637238aec26693a940709a5bf2afa61c0300061
Maxim Ladoshin <mladoshin@mail.ru>
2024-09-18 00:02:53 +0300

    fix: fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
+#!/bin/sh
+mode=$1
+if [ -z "$mode" ] 
+echo Error! Select build mode: build or staging
+then exit 1
+fi
+
 git reset --hard
 git pull
-npm run build
+npm run build:$mode
 rm -rf /usr/share/nginx/admin_frontend
 cp -r dist/ /usr/share/nginx/admin_frontend
 echo Successful
